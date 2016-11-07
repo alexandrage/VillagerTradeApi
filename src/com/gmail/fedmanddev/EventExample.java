d11ed8cb51c70aeae9d00ddcd6b5d6e491ac4d64
alexandrage <alexandrage@mail.ru>
2016-11-07 05:25:51 +0300

    added create fake Villager
    added create fake Villager

--- src/com/gmail/fedmanddev/EventExample.java
+++ src/com/gmail/fedmanddev/EventExample.java
@@ -2,10 +2,12 @@ package com.gmail.fedmanddev;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class EventExample implements Listener {
@@ -14,18 +16,29 @@ public class EventExample implements Listener {
 		this.plugin = instance;
 	}
 	
-	//Usage
+	//Example 1
 	@EventHandler
     public void on(PlayerInteractEntityEvent e) {
 		Entity ent =  e.getRightClicked();
 		if(ent instanceof Villager) {
-			Villager villager = (Villager)  ent;
+			Villager villager = (Villager)ent;
 			VillagerTradeApi.clearTrades(villager);
-			ItemStack item1 = new ItemStack(Material.GOLD_NUGGET, 10);
-			ItemStack item2 = new ItemStack(Material.APPLE);
-			ItemStack reward = new ItemStack(Material.DIAMOND_HOE);
-			VillagerTrade trade = new VillagerTrade(item1, item2, reward);
-			VillagerTradeApi.addTrade(villager, trade);
+			ItemStack item1 = new ItemStack(Material.APPLE);
+			ItemStack reward = new ItemStack(Material.GOLD_NUGGET);
+			VillagerTrade trade = new VillagerTrade(item1, reward);
+			VillagerTradeApi.addTrade(villager, trade, Integer.MAX_VALUE);
 		}
+	}
+	
+	//Example 2
+	@EventHandler
+    public void on(PlayerInteractEvent e) {
+		Player p = e.getPlayer();
+		Villager villager = VillagerTradeApi.createVillager(p);
+		ItemStack item1 = new ItemStack(Material.APPLE);
+		ItemStack reward = new ItemStack(Material.GOLD_NUGGET);
+		VillagerTrade trade = new VillagerTrade(item1, reward);
+		VillagerTradeApi.addTrade(villager, trade, Integer.MAX_VALUE);
+		e.getPlayer().openMerchant(villager, true);
 	}
 }
