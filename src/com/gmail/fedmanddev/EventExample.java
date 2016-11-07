1aa2e3f06cf4553807dbe79f4649438592496d9b
alexandrage <alexandrage@mail.ru>
2016-11-07 03:16:49 +0300

    EventExample
    EventExample

--- src/com/gmail/fedmanddev/EventExample.java
+++ src/com/gmail/fedmanddev/EventExample.java
@@ -1,6 +1,7 @@
 package com.gmail.fedmanddev;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,15 +13,19 @@ public class EventExample implements Listener {
 	public EventExample(VillagerTradeApi instance) {
 		this.plugin = instance;
 	}
-
+	
+	//Usage
 	@EventHandler
     public void on(PlayerInteractEntityEvent e) {
-		Villager villager = (Villager)  e.getRightClicked();
-		VillagerTradeApi.clearTrades(villager);
-		ItemStack item1 = new ItemStack(Material.GOLD_NUGGET, 10);
-		ItemStack item2 = new ItemStack(Material.APPLE);
-		ItemStack reward = new ItemStack(Material.DIAMOND_HOE);
-		VillagerTrade trade = new VillagerTrade(item1, item2, reward);
-		VillagerTradeApi.addTrade(villager, trade);
+		Entity ent =  e.getRightClicked();
+		if(ent instanceof Villager) {
+			Villager villager = (Villager)  e.getRightClicked();
+			VillagerTradeApi.clearTrades(villager);
+			ItemStack item1 = new ItemStack(Material.GOLD_NUGGET, 10);
+			ItemStack item2 = new ItemStack(Material.APPLE);
+			ItemStack reward = new ItemStack(Material.DIAMOND_HOE);
+			VillagerTrade trade = new VillagerTrade(item1, item2, reward);
+			VillagerTradeApi.addTrade(villager, trade);
+		}
 	}
 }
