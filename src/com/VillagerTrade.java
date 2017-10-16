6b6334f35116a58db2e6e8952a3fc4f9bdb24206
alexandrage <alexandrage@mail.ru>
2017-10-16 17:07:21 +0300

    up 1.12

--- src/com/VillagerTrade.java
+++ src/com/VillagerTrade.java
@@ -3,34 +3,34 @@ package com;
 import org.bukkit.inventory.ItemStack;
 
 public final class VillagerTrade {
-  private ItemStack item1;
-  private ItemStack item2;
-  private ItemStack rewardItem;
-  
-  public VillagerTrade(ItemStack item1, ItemStack item2, ItemStack rewardItem) {
-    this.item1 = item1;
-    this.item2 = item2;
-    this.rewardItem = rewardItem;
-  }
-  
-  public VillagerTrade(ItemStack item1, ItemStack rewardItem) {
-    this.item1 = item1;
-    this.rewardItem = rewardItem;
-  }
-  
-  public static boolean hasItem2(VillagerTrade villagerTrade) {
-    return villagerTrade.item2 != null;
-  }
-  
-  public static ItemStack getItem1(VillagerTrade villagerTrade) {
-    return villagerTrade.item1;
-  }
-  
-  public static ItemStack getItem2(VillagerTrade villagerTrade) {
-    return villagerTrade.item2;
-  }
-  
-  public static ItemStack getRewardItem(VillagerTrade villagerTrade) {
-    return villagerTrade.rewardItem;
-  }
+	private ItemStack item1;
+	private ItemStack item2;
+	private ItemStack rewardItem;
+
+	public VillagerTrade(ItemStack item1, ItemStack item2, ItemStack rewardItem) {
+		this.item1 = item1;
+		this.item2 = item2;
+		this.rewardItem = rewardItem;
+	}
+
+	public VillagerTrade(ItemStack item1, ItemStack rewardItem) {
+		this.item1 = item1;
+		this.rewardItem = rewardItem;
+	}
+
+	public static boolean hasItem2(VillagerTrade villagerTrade) {
+		return villagerTrade.item2 != null;
+	}
+
+	public static ItemStack getItem1(VillagerTrade villagerTrade) {
+		return villagerTrade.item1;
+	}
+
+	public static ItemStack getItem2(VillagerTrade villagerTrade) {
+		return villagerTrade.item2;
+	}
+
+	public static ItemStack getRewardItem(VillagerTrade villagerTrade) {
+		return villagerTrade.rewardItem;
+	}
 }
--- src/com/VillagerTradeApi.java
+++ src/com/VillagerTradeApi.java
@@ -1,77 +1,95 @@
 package com;
 
 import java.lang.reflect.Field;
-import net.minecraft.server.v1_10_R1.EntityVillager;
-import net.minecraft.server.v1_10_R1.ItemStack;
-import net.minecraft.server.v1_10_R1.MerchantRecipe;
-import net.minecraft.server.v1_10_R1.MerchantRecipeList;
-import net.minecraft.server.v1_10_R1.World;
-
-import org.bukkit.craftbukkit.v1_10_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_10_R1.entity.CraftVillager;
-import org.bukkit.craftbukkit.v1_10_R1.inventory.CraftItemStack;
+import net.minecraft.server.v1_12_R1.EntityVillager;
+import net.minecraft.server.v1_12_R1.ItemStack;
+import net.minecraft.server.v1_12_R1.MerchantRecipe;
+import net.minecraft.server.v1_12_R1.MerchantRecipeList;
+import net.minecraft.server.v1_12_R1.World;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftVillager;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class VillagerTradeApi extends JavaPlugin {
-  public void onEnable() {
-    getLogger().info("[VillagerTradeApi] Enabled");
-    //Example used
-    //getServer().getPluginManager().registerEvents(new EventExample(this), this);
-  }
-  
-  public static void clearTrades(Villager villager) {
-    EntityVillager entityVillager = ((CraftVillager)villager).getHandle();
-    try {
-      //EntityVillager.class: Field -> private MerchantRecipeList trades;
-      Field recipes = entityVillager.getClass().getDeclaredField("trades");
-      recipes.setAccessible(true);
-      MerchantRecipeList list = new MerchantRecipeList();
-      recipes.set(entityVillager, list);
-    }
-    catch (Exception exc) {
-      exc.printStackTrace();
-    }
-  }
-  
-  public static void addTrade(Villager villager, VillagerTrade villagerTrade, int maxUses) {
-    EntityVillager entityVillager = ((CraftVillager)villager).getHandle();
-    try {
-      //EntityVillager.class: Field -> private MerchantRecipeList trades;
-      Field recipes = entityVillager.getClass().getDeclaredField("trades");
-      recipes.setAccessible(true);
-      MerchantRecipeList list = (MerchantRecipeList)recipes.get(entityVillager);
-      if (VillagerTrade.hasItem2(villagerTrade)) {
-        ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getItem1(villagerTrade));
-        ItemStack item2 = CraftItemStack.asNMSCopy(VillagerTrade.getItem2(villagerTrade));
-        ItemStack rewardItem = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
-        MerchantRecipe recipe = new MerchantRecipe(item1, item2, rewardItem);
-        recipe.maxUses = maxUses;
-        list.add(recipe);
-      } else {
-        ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getItem1(villagerTrade));
-        ItemStack rewardItem = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
-        MerchantRecipe recipe = new MerchantRecipe(item1, rewardItem);
-        recipe.maxUses = maxUses;
-        list.add(recipe);
-      }
-      recipes.set(entityVillager, list);
-    }
-    catch (Exception exc) {
-      exc.printStackTrace();
-    }
-  }
-  
-  public static void addTrade(Villager villager, VillagerTrade villagerTrade) {
-	  addTrade(villager, villagerTrade, 7);
-  }
-  
-  public static Villager createVillager(Player p) {
-	    World world = ((CraftWorld) p.getWorld()).getHandle();
+public class VillagerTradeApi extends JavaPlugin {
+	public void onEnable() {
+		getLogger().info("[VillagerTradeApi] Enabled");
+	}
+
+	public void clearTrades(Villager villager) {
+		EntityVillager entityVillager = ((CraftVillager) villager).getHandle();
+		try {
+			Field recipes = entityVillager.getClass().getDeclaredField("trades");
+			recipes.setAccessible(true);
+			MerchantRecipeList list = new MerchantRecipeList();
+			recipes.set(entityVillager, list);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	public void addTrade(Villager villager, org.bukkit.inventory.ItemStack item1,
+			org.bukkit.inventory.ItemStack rewardItem) {
+		addTrade(villager, item1, null, rewardItem, 7);
+	}
+
+	public void addTrade(Villager villager, org.bukkit.inventory.ItemStack item1, org.bukkit.inventory.ItemStack item2,
+			org.bukkit.inventory.ItemStack rewardItem) {
+		addTrade(villager, item1, item2, rewardItem, 7);
+	}
+
+	public void addTrade(Villager villager, org.bukkit.inventory.ItemStack item1,
+			org.bukkit.inventory.ItemStack rewardItem, int maxUses) {
+		addTrade(villager, item1, null, rewardItem, 7);
+	}
+
+	public void addTrade(Villager villager, org.bukkit.inventory.ItemStack stack1,
+			org.bukkit.inventory.ItemStack stack2, org.bukkit.inventory.ItemStack stack3, int maxUses) {
+		EntityVillager entityVillager = ((CraftVillager) villager).getHandle();
+		VillagerTrade villagerTrade = setMerchant(stack1, stack2, stack3);
+		try {
+			Field recipes = entityVillager.getClass().getDeclaredField("trades");
+			recipes.setAccessible(true);
+			MerchantRecipeList list = (MerchantRecipeList) recipes.get(entityVillager);
+			if (VillagerTrade.hasItem2(villagerTrade)) {
+				ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getItem1(villagerTrade));
+				ItemStack item2 = CraftItemStack.asNMSCopy(VillagerTrade.getItem2(villagerTrade));
+				ItemStack rewardItem = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
+				MerchantRecipe recipe = new MerchantRecipe(item1, item2, rewardItem);
+				recipe.maxUses = maxUses;
+				list.add(recipe);
+			} else {
+				ItemStack item1 = CraftItemStack.asNMSCopy(VillagerTrade.getItem1(villagerTrade));
+				ItemStack rewardItem = CraftItemStack.asNMSCopy(VillagerTrade.getRewardItem(villagerTrade));
+				MerchantRecipe recipe = new MerchantRecipe(item1, rewardItem);
+				recipe.maxUses = maxUses;
+				list.add(recipe);
+			}
+			recipes.set(entityVillager, list);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	public Villager createVillager(Player p) {
+		World world = ((CraftWorld) p.getWorld()).getHandle();
 		EntityVillager entityvillager = new EntityVillager(world);
 		CraftVillager villager = new CraftVillager(null, entityvillager);
 		clearTrades(villager);
 		return villager;
-  }
+	}
+
+	private VillagerTrade setMerchant(org.bukkit.inventory.ItemStack item1, org.bukkit.inventory.ItemStack item2,
+			org.bukkit.inventory.ItemStack rewardItem) {
+		if (item2 == null) {
+			return new VillagerTrade(item1, rewardItem);
+		}
+		return new VillagerTrade(item1, item2, rewardItem);
+	}
+
+	public void openMerchant(Player p, Villager villager) {
+		p.openMerchant(villager, true);
+	}
 }
